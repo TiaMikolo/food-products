a14cff32495a88e6788fda9d6ecef018df307eb6
Mikolo <Mikolorah@gmail.com>
2026-02-23 16:59:07 +0300

    modification of middlewares file

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,14 +1,14 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; //ex : ["Bearer", "TOKEN"] => take TOKEN
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  const token = req.headers.authorization?.split(' ')[1] //ex : ["Bearer", "TOKEN"] => take TOKEN
+  if (!token) return res.status(401).json({ message: 'Unauthorized' })
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); //verify token
-    req.user = decoded; //add user info to request
-    next();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) //verify token
+    req.user = decoded //add user info to request
+    next()
   } catch {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: 'Unauthorized' })
   }
-};
+}
