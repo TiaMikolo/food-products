b0a0b0f9c80f61323802fe913b15bf275e55b304
Mikolo <Mikolorah@gmail.com>
2026-03-09 08:13:06 +0300

    amelioration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,19 @@
+import React from 'react'
+import '../global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
- import '../global.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import ProductListPage from './pages/ProductListPage/ProductListPage.jsx'
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx'
+import SignInPage from './pages/SignInPage/SignInPage.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignUpPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/products" element={<ProductListPage />} />
+      <Route path="/signin" element={<SignInPage />} />
+    </Routes>
+  </BrowserRouter>
+)
